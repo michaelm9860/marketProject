3febd45cf20b7081ebf5a5c59b39e69ca3741f4e
MichaelM <michaelm10019860@gmail.com>
2024-06-06 19:08:38 +0300

    Added max value to user password

--- src/main/java/michael/m/marketProject/dto/user_dto/UserCreateDTO.java
+++ src/main/java/michael/m/marketProject/dto/user_dto/UserCreateDTO.java
@@ -41,7 +41,7 @@ public class UserCreateDTO {
     private String location;
 
     @NotNull
-    @Size(min = 8, message = "Password should have at least 8 characters")
+    @Size(min = 8, max = 20, message = "Password should have between 8 and 20 characters")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$", message = "password must contain at least 1 lowercase letter, 1 uppercase letter, 1 digit and 1 special character")
     private String password;
 
