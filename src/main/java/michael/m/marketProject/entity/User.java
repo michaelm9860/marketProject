7af2a9779bb949004f483ebba17c3a90dbf86bd8
MichaelM <michaelm10019860@gmail.com>
2024-06-06 20:37:41 +0300

    Added max value to user password

--- src/main/java/michael/m/marketProject/entity/User.java
+++ src/main/java/michael/m/marketProject/entity/User.java
@@ -67,7 +67,7 @@ public class User {
 
     @NotNull
     @Size(min = 8, max = 20, message = "Password should have between 8 and 20 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$", message = "password must contain at least 1 lowercase letter, 1 uppercase letter, 1 digit and 1 special character")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,20}$", message = "password must contain at least 1 lowercase letter, 1 uppercase letter, 1 digit and 1 special character")
     private String password;
 
     @CreationTimestamp
